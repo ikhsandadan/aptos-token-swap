5fafca3a58e9a2eb7489ecd0f03e56bb8f1731a3
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2024-05-27 13:04:35 +0700

    Update page.tsx

--- app/Frontpage/page.tsx
+++ app/Frontpage/page.tsx
@@ -112,7 +112,7 @@ const Frontpage = () => {
     }, [userAddress]);
 
     return (
-        <div style={{minHeight: '75vh'}}>
+        <div style={{minHeight: '60vh'}}>
             {userAddress ? 
             <div className='grid grid-flow-row auto-rows-auto gap-10'>
                 <div className='text-center font-bold text-xl pt-2'>Aptos Swap Token</div>
